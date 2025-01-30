db8eb0429c27c43fb30f8f7eac776c98af90c92c
Bamcane <diandaokui@qq.com>
2025-01-30 22:13:45 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Teeworlds-Data
+==============
+一个仓库用于自动更新一些teeworlds相关资源
+
+# 提供列表
+DDNet(Android)
+TaterClient(Windows)
+InfClass-Client(Windows)
---
+++ build.py
@@ -0,0 +1,39 @@
+import sys, os, markdown
+import shutil
+
+def BuildWeb():
+    if os.path.exists("web-build") == False:
+        os.mkdir("web-build")
+
+    # 读取模板
+    file = open("example.html")
+    example = file.read()
+    file.close()
+
+    if os.path.exists("web-build/resources"):
+        shutil.rmtree("web-build/resources")
+    shutil.copytree("resources", "web-build/resources")
+
+    def BuildFile(name):
+        # 转换HTML
+        file = open(f"docs/{name}.md")
+        md = file.read()
+        md.replace(".md", ".html") # 暂时先这样判断一下（
+        html = markdown.markdown(md)
+        file.close()
+        # 保存
+        buffer = example
+        buffer = buffer.format(html)
+        file = open(f"web-build/{name}.html", "w")
+        file.write(buffer)
+        file.close()
+    for file_path in os.listdir("docs"):
+        file_name = os.path.splitext(file_path)[0]
+        BuildFile(file_name)
+
+
+def main():
+    BuildWeb()
+
+if __name__ == "__main__":
+    sys.exit(main())
